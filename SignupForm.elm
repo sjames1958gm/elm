200b55d845505c38037cdc4b7de06ab196eb476b
Stephen James <sajames1958@gmail.com>
2016-09-29 21:10:43 -0500

    Update SignupForm.elm

--- SignupForm.elm
+++ SignupForm.elm
@@ -1,29 +1,146 @@
-var express = require("express");
+module SignupForm exposing (..)
 
-var app = express();
+-- This is where our Elm logic lives.`module SignupForm` declares that this is
+-- the SignupForm module, which is how other modules will reference this one
+-- if they want to import it and reuse its code.
 
-app.set('port', (process.env.PORT || 8080));
+import Html.App
 
-console.log("Starting server on: " + app.get('port'));
 
-app.use(function(req, res, next) {
-  console.log("request");
-  next();
-});
+-- Elm’s "import" keyword works similarly to "require" in node.js.
 
-app.use(function(req, res, next) {
-  console.log("here: " + JSON.stringify(req.headers));
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    console.log("redirecting to: " + 'http://' + req.hostname + req.url);
-    res.redirect('http://' + req.hostname + req.url);
-  }
-  else {
-    next();
-  }
-});
+import Html exposing (..)
 
-app.use(express.static(__dirname + '/public'));
 
-app.listen(app.get('port'), function() {
-    console.log("server started on " + app.get('port'));
-});
+-- The “exposing (..)” option says that we want to bring the Html module’s contents
+-- into this file’s current namespace, so that instead of writing out
+-- Html.form and Html.label we can use "form" and "label" without the "Html."
+
+import Html.Events exposing (..)
+
+
+-- This works the same way; we also want to import the entire
+-- Html.Events module into the current namespace.
+
+import Html.Attributes exposing (id, type', for, value, class)
+
+
+-- With this import we are only bringing a few specific functions into our
+-- namespace, specifically "id", "type'", "for", "value", and "class".
+
+import Http
+import Task exposing (Task)
+import Json.Decode exposing (succeed)
+
+
+view model =
+    form [ id "signup-form" ]
+        [ h1 [] [ text "Sensational Signup Form" ]
+        , label [ for "username-field" ] [ text "username: " ]
+        , input
+            [ id "username-field"
+            , type' "text"
+            , value model.username
+            , onInput (\str -> { msgType = "SET_USERNAME", payload = str })
+            ]
+            []
+        , div [ class "validation-error" ] [ text (viewUsernameErrors model) ]
+        , label [ for "password" ] [ text "password: " ]
+        , input
+            [ id "password-field"
+            , type' "password"
+            , value model.password
+            , onInput (\str -> { msgType = "SET_PASSWORD", payload = str })
+            ]
+            []
+        , div [ class "validation-error" ] [ text model.errors.password ]
+        , div [ class "signup-button", onClick { msgType = "VALIDATE", payload = "" } ] [ text "Sign Up!" ]
+        ]
+
+
+viewUsernameErrors model =
+    if model.errors.usernameTaken then
+        "That username is taken!"
+    else
+        model.errors.username
+
+
+
+-- Take a look at this starting model we’re passing to our view function.
+-- Note that in Elm syntax, we use = to separate fields from values
+-- instead of : like JavaScript uses for its object literals.
+
+
+getErrors model =
+    { username =
+        if model.username == "" then
+            "Please enter a username!"
+        else
+            ""
+    , password =
+        if model.password == "" then
+            "Please enter a password!"
+        else
+            ""
+    , usernameTaken = model.errors.usernameTaken
+    }
+
+
+update msg model =
+    if msg.msgType == "VALIDATE" then
+        let
+            url =
+                "https://api.github.com/users/" ++ model.username
+
+            failureToMsg err =
+                { msgType = "USERNAME_AVAILABLE", payload = "" }
+
+            successToMsg result =
+                { msgType = "USERNAME_TAKEN", payload = "" }
+
+            request =
+                Http.get (succeed "") url
+
+            cmd =
+                Task.perform failureToMsg successToMsg request
+        in
+            ( { model | errors = getErrors model }, cmd )
+    else if msg.msgType == "SET_USERNAME" then
+        ( { model | username = msg.payload }, Cmd.none )
+    else if msg.msgType == "SET_PASSWORD" then
+        ( { model | password = msg.payload }, Cmd.none )
+    else if msg.msgType == "USERNAME_TAKEN" then
+        ( withUsernameTaken True model, Cmd.none )
+    else if msg.msgType == "USERNAME_AVAILABLE" then
+        ( withUsernameTaken False model, Cmd.none )
+    else
+        ( model, Cmd.none )
+
+
+withUsernameTaken isTaken model =
+    let
+        currentErrors =
+            model.errors
+
+        newErrors =
+            { currentErrors | usernameTaken = isTaken }
+    in
+        { model | errors = newErrors }
+
+
+initialErrors =
+    { username = "", password = "", usernameTaken = False }
+
+
+initialModel =
+    { username = "", password = "", errors = initialErrors }
+
+
+main =
+    Html.App.program
+        { init = ( initialModel, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
